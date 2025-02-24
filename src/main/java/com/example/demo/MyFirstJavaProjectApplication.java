0a66954321dbdf3c9513676c6f0a51890d7c7d1d
yasuda <eriko@csplan.jp>
2025-02-24 16:02:52 +0900

    コントローラの作成

--- src/main/java/com/example/demo/MyFirstJavaProjectApplication.java
+++ src/main/java/com/example/demo/MyFirstJavaProjectApplication.java
@@ -2,12 +2,13 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class MyFirstJavaProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyFirstJavaProjectApplication.class, args);
 	}
-
 }
