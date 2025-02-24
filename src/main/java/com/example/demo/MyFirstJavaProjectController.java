efc324fc09400c616c85aa6726cc917ec181cf13
yasuda <eriko@csplan.jp>
2025-02-24 16:34:46 +0900

    ２－４ コントローラとビューの連携

--- src/main/java/com/example/demo/MyFirstJavaProjectController.java
+++ src/main/java/com/example/demo/MyFirstJavaProjectController.java
@@ -1,12 +1,19 @@
 package com.example.demo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MyFirstJavaProjectController {
-	@RequestMapping("/")
-	public String index() {
+	@RequestMapping("/{num}")
+	public String index(@PathVariable int num, Model model) {
+		int sum = 0;
+		for(int i = 1 ; i <= num ; i++) {
+			sum += i;
+		}
+		model.addAttribute("msg", "sum=" + sum);
 		return "index";
 	}
 }
